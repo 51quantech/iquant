8ea81efc4de8c9f9c565a659721b9f26d9911dd1
yonggangli <yonggangli@sohu-inc.com>
2016-09-28 18:00:22 +0800

    引入了springboot

--- src/main/java/com/iquant/controller/HelpController.java
+++ src/main/java/com/iquant/controller/HelpController.java
@@ -1,10 +1,8 @@
 package com.iquant.controller;
 
-import com.iquant.service.UserService;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,9 +18,6 @@ import java.util.Map;
 public class HelpController {
 
     private final static Logger LOG = Logger.getLogger(HelpController.class);
-
-    @Autowired
-    private UserService userService;
 
     @RequestMapping(value = "/data")
     public String data() {
